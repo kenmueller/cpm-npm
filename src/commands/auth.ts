67152321f2d837c7ab976603242f5f3bddf7d354
kenmueller <kenmueller0@gmail.com>
2020-06-25 18:09:30 -0700

    auth done

--- src/commands/auth.ts
+++ src/commands/auth.ts
@@ -5,12 +5,15 @@ import { setAuth } from '../utils/auth'
 
 export default async (email: string, password: string) => {
 	try {
+		if (!(email && password))
+			return console.log(chalk.red.bold('Your email or password cannot be empty'))
+		
 		const { data } = await axios.post('/auth', { email, password })
 		
 		await setAuth(data.uid, email, data.password)
 		
 		console.log(chalk.green.bold('Successfully logged in'))
-	} catch (error) {
-		console.log(error)
+	} catch ({ response: { data } }) {
+		console.log(chalk.red.bold(data))
 	}
 }
